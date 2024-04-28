505eeb2159f250cc7dd199fd43479eb5897e2fe8
Vaibhav Shivaji Bhosale <textmevaibhav@gmail.com>
2024-04-28 17:43:31 +0530

    .

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port=process.env.PORT || 8008
 const app=express()
 
 
-mongoose.connect(process.env.MONGO_URL).
+mongoose.connect("mongodb://localhost:27017/blogify").
 then(e=>console.log("mongodb connected"))
 
 
